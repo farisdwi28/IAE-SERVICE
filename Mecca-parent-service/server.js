bed24df38fb17e80fa0b993a51bfb5d3da323653
Shalman Rafli Alva Rizqi <shalmanrafli.asli@gmail.com>
2025-12-26 00:06:07 +0700

    update: add route in server.js for teacher sync

--- Mecca-parent-service/server.js
+++ Mecca-parent-service/server.js
@@ -21,6 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.post('/api/sync/students', syncController.syncStudentData);
 app.post('/api/sync/classes', syncController.syncClass);
+app.post('/api/sync/teacher', syncController.syncTeacher);
 
 // Routes
 app.use('/api/parent', parentRoutes);
