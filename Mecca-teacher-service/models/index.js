68bf6447af88fdfbf25068b876bf88c2b198386f
Shalman Rafli Alva Rizqi <shalmanrafli.asli@gmail.com>
2026-01-03 16:55:21 +0700

    Update on teacher service: add new relation models

--- Mecca-teacher-service/models/index.js
+++ Mecca-teacher-service/models/index.js
@@ -1,71 +1,105 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database'); // Pastikan path ini benar mengarah ke instance Sequelize
+const sequelize = require('../config/database');
 
-// 1. Inisialisasi Model (Panggil Function-nya)
-const Admin = require('./Admin')(sequelize, DataTypes);
-const Student = require('./Student')(sequelize, DataTypes);
-const Teacher = require('./Teacher')(sequelize, DataTypes);
-const Class = require('./Class')(sequelize, DataTypes);
-const Subject = require('./Subject')(sequelize, DataTypes);
-const Schedule = require('./Schedule')(sequelize, DataTypes);
-const Attendance = require('./Attendance')(sequelize, DataTypes);
-const Grade = require('./Grade')(sequelize, DataTypes);
-const Fee = require('./Fee')(sequelize, DataTypes);
-const Bill = require('./Bill')(sequelize, DataTypes);
-const LibraryBook = require('./LibraryBook')(sequelize, DataTypes);
-const LibraryLoan = require('./LibraryLoan')(sequelize, DataTypes);
+// --- Helper untuk load model yang formatnya campur-campur ---
+const loadModel = (modelName) => {
+    const modelDef = require(`./${modelName}`);
+    
+    // Coba panggil sebagai fungsi (Factory Pattern)
+    try {
+        return modelDef(sequelize, DataTypes);
+    } catch (error) {
+        // Jika error "Class constructor", berarti dia Model Class (Legacy) -> Pakai langsung
+        if (error.message.includes("Class constructor") || typeof modelDef !== 'function') {
+            return modelDef;
+        }
+        throw error;
+    }
+};
+
+// 1. Load Semua Model dengan Helper
+const Admin = loadModel('Admin');
+const Student = loadModel('Student');
+const Teacher = loadModel('Teacher');
+const Class = loadModel('Class');
+const Subject = loadModel('Subject');
+const Schedule = loadModel('Schedule');
+const Attendance = loadModel('Attendance');
+const Grade = loadModel('Grade');
+const Fee = loadModel('Fee');
+const Bill = loadModel('Bill');
+const LibraryBook = loadModel('LibraryBook');
+const LibraryLoan = loadModel('LibraryLoan');
 
 // 2. Definisi Relasi (Associations)
 
-// Class - Student
-Class.hasMany(Student, { foreignKey: 'classId' });
-Student.belongsTo(Class, { foreignKey: 'classId' });
+// Class & Student
+if (Class && Student) {
+    Class.hasMany(Student, { foreignKey: 'classId' });
+    Student.belongsTo(Class, { foreignKey: 'classId' });
+}
 
-// Class - Schedule
-Class.hasMany(Schedule, { foreignKey: 'classId' });
-Schedule.belongsTo(Class, { foreignKey: 'classId' });
+// Class & Schedule
+if (Class && Schedule) {
+    Class.hasMany(Schedule, { foreignKey: 'classId' });
+    Schedule.belongsTo(Class, { foreignKey: 'classId' });
+}
 
-// Subject - Schedule
-Subject.hasMany(Schedule, { foreignKey: 'subjectId' });
-Schedule.belongsTo(Subject, { foreignKey: 'subjectId' });
+// Subject & Schedule
+if (Subject && Schedule) {
+    Subject.hasMany(Schedule, { foreignKey: 'subjectId' });
+    Schedule.belongsTo(Subject, { foreignKey: 'subjectId' });
+}
 
-// Teacher - Schedule
-Teacher.hasMany(Schedule, { foreignKey: 'teacherId' });
-Schedule.belongsTo(Teacher, { foreignKey: 'teacherId' });
+// Teacher & Schedule
+if (Teacher && Schedule) {
+    Teacher.hasMany(Schedule, { foreignKey: 'teacherId' });
+    Schedule.belongsTo(Teacher, { foreignKey: 'teacherId' });
+}
 
-// Student - Attendance
-Student.hasMany(Attendance, { foreignKey: 'studentId' });
-Attendance.belongsTo(Student, { foreignKey: 'studentId' });
+// Student & Attendance
+if (Student && Attendance) {
+    Student.hasMany(Attendance, { foreignKey: 'studentId' });
+    Attendance.belongsTo(Student, { foreignKey: 'studentId' });
+}
 
-// Schedule - Attendance
-Schedule.hasMany(Attendance, { foreignKey: 'scheduleId' });
-Attendance.belongsTo(Schedule, { foreignKey: 'scheduleId' });
+// Schedule & Attendance
+if (Schedule && Attendance) {
+    Schedule.hasMany(Attendance, { foreignKey: 'scheduleId' });
+    Attendance.belongsTo(Schedule, { foreignKey: 'scheduleId' });
+}
 
 // --- RELASI PENTING UNTUK NILAI ---
-// Student - Grade
-Student.hasMany(Grade, { foreignKey: 'studentId' });
-Grade.belongsTo(Student, { foreignKey: 'studentId' });
+if (Student && Grade) {
+    Student.hasMany(Grade, { foreignKey: 'studentId' });
+    Grade.belongsTo(Student, { foreignKey: 'studentId' });
+}
 
-// Subject - Grade
-Subject.hasMany(Grade, { foreignKey: 'subjectId' });
-Grade.belongsTo(Subject, { foreignKey: 'subjectId' });
+if (Subject && Grade) {
+    Subject.hasMany(Grade, { foreignKey: 'subjectId' });
+    Grade.belongsTo(Subject, { foreignKey: 'subjectId' });
+}
 // ----------------------------------
 
-// Student - Bill
-Student.hasMany(Bill, { foreignKey: 'studentId' });
-Bill.belongsTo(Student, { foreignKey: 'studentId' });
+// Bills & Fees
+if (Student && Bill) {
+    Student.hasMany(Bill, { foreignKey: 'studentId' });
+    Bill.belongsTo(Student, { foreignKey: 'studentId' });
+}
+if (Fee && Bill) {
+    Fee.hasMany(Bill, { foreignKey: 'feeId' });
+    Bill.belongsTo(Fee, { foreignKey: 'feeId' });
+}
 
-// Fee - Bill
-Fee.hasMany(Bill, { foreignKey: 'feeId' });
-Bill.belongsTo(Fee, { foreignKey: 'feeId' });
-
-// Student - LibraryLoan
-Student.hasMany(LibraryLoan, { foreignKey: 'studentId' });
-LibraryLoan.belongsTo(Student, { foreignKey: 'studentId' });
-
-// LibraryBook - LibraryLoan
-LibraryBook.hasMany(LibraryLoan, { foreignKey: 'bookId' });
-LibraryLoan.belongsTo(LibraryBook, { foreignKey: 'bookId' });
+// Library
+if (Student && LibraryLoan) {
+    Student.hasMany(LibraryLoan, { foreignKey: 'studentId' });
+    LibraryLoan.belongsTo(Student, { foreignKey: 'studentId' });
+}
+if (LibraryBook && LibraryLoan) {
+    LibraryBook.hasMany(LibraryLoan, { foreignKey: 'bookId' });
+    LibraryLoan.belongsTo(LibraryBook, { foreignKey: 'bookId' });
+}
 
 // 3. Export Semua
 module.exports = {
